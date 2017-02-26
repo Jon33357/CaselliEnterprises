fe52d482984b9522a9611d8659e3a1aecf0f4757
lucas2010 <luis.caselli@gmail.com>
2017-02-26 14:24:37 +0000

    WCF implementation working progress

--- Code/Accounting/master/AccountingWcfServices/AccountingWcfServices/Code/DAL/DAL.cs
+++ Code/Accounting/master/AccountingWcfServices/AccountingWcfServices/Code/DAL/DAL.cs
@@ -5,7 +5,7 @@
     public class DAL {
         public int populateModel() {
             using (var accountingEntities = new AccountingEntities()) {
-
+                accountingEntities.account_document
             }
 
             return 0;
---
+++ Code/DMS/master/DMSWcfServices/DMSWcfServices/Code/BusinessLogicLayer/CoreServiceManager.cs
@@ -0,0 +1,61 @@
+﻿using DMSWcfServices.Code.DAL.EF;
+using DMSWcfServices.Code.ServiceLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DMSWcfServices.Code.BusinessLogicLayer
+{
+    public class CoreServiceManager
+    {
+        //still need to implement dependency injection
+        ProductionManager prodManager;
+        PageManager pageManager;
+
+        //still need to implement transactionality in DB access
+        public ServiceInvocationResult putProductionUnstructured(Production prod, bool? allowUpdates)
+        {
+            if (prod == null) throw new ArgumentException("prod argument cannot be null!", "prod");
+
+            bool _allowUpdates = false;
+            if (allowUpdates != null && allowUpdates==true)
+                _allowUpdates = true;
+
+
+            //first create production
+            try{
+                prodManager.createProduction(prod);
+            }
+            catch (Exception ex) {//replace with appropriate exception to catch duplicate primary key exception
+                if (_allowUpdates)
+                    prodManager.updateProduction(prod);
+                else
+                    return new ServiceInvocationResult((int)ServiceInvocationResultCode.UpdatesNotAllowed,
+                        "production " + prod.id + " already exist in system and allowUpdates not set to true!",
+                        ex.Message, ex);
+            }
+
+            //then create pages
+            if (prod.Page != null && prod.Page.Count > 0) {
+                foreach (Page page in prod.Page) {
+                    try{
+                        pageManager.createPage(page);
+                    }
+                    catch (Exception ex){//replace with appropriate exception to catch duplicate primary key exception
+                        if (_allowUpdates)
+                            pageManager.updatePage(page);
+                        else
+                            return new ServiceInvocationResult((int)ServiceInvocationResultCode.UpdatesNotAllowed,
+                                "page with produstionId=" + page.production_id + " and pageNumber=" + page.page_number + " already exist in system and allowUpdates not set to true!",
+                                ex.Message, ex);
+                    }
+                }
+            }
+
+
+            return new ServiceInvocationResult((int)ServiceInvocationResultCode.OK, null, null, null);
+        }
+
+    }
+}
